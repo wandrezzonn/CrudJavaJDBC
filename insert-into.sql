4dd3d9dc0aa5f13b95e2f7734d331b63829803c5
Anderson Silva <anderson4602@gmail.com>
2021-01-04 09:05:31 -0300

    excluido arquivo slq

--- insert-into.sql
+++
@@ -1,7 +0,0 @@
-insert into cliente (nome_cliente,cpf_cliente,data_nasci_cliente,tipo_cliente) values ('FHF', '78965412302','1980-01-05 01:03:25',2);
-
-insert into cliente values(6,'MRV','11111123654','2021-01-01 12:51:30',2);
-
-insert into tipo_cliente (desc_tipo_cliente) value('Pessoa Jurídica');
-
-insert into venda values (8,1,'2019-11-01',685.0);
